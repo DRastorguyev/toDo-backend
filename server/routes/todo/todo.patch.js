d1fd36d0513692672dab689dfb0f950a621820db
daniil <daniil.alpaca@gmail.com>
2021-09-15 15:17:58 +0300

    change name of variables

--- server/routes/todo/todo.patch.js
+++ server/routes/todo/todo.patch.js
@@ -5,18 +5,18 @@ const router = Router();
 
 router.patch('/todos/:uuid', async (req, res) => {
   let todos = readTodos(); // get the elements from the array
-  const data = ['name', 'done'];
+  const fields = ['name', 'done'];
   const { uuid } = req.params;
-  const editData = {};
+  const editFields = {};
 
   const editedIndex = todos.findIndex((todo) => todo.uuid === uuid);
 
   try {
-    data.forEach((propertyName) => {
+    fields.forEach((propertyName) => {
       if (propertyName in req.body)
-        editData[propertyName] = req.body[propertyName];
+        editFields[propertyName] = req.body[propertyName];
     });
-    todos[editedIndex] = { ...todos[editedIndex], ...editData };
+    todos[editedIndex] = { ...todos[editedIndex], ...editFields };
     await writeTodo(todos);
     res.send(todos[editedIndex]);
   } catch (e) {
