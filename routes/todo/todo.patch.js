2e31eeb60fcd7494101f7e3382b20451afcd5593
daniil <daniil.alpaca@gmail.com>
2021-10-27 11:26:02 +0300

    fix heroku

--- routes/todo/todo.patch.js
+++ routes/todo/todo.patch.js
@@ -11,54 +11,15 @@ router.patch('/todos/:id', authMiddlware, async (req, res) => {
   const { title, done, selectedTodoId, targetTodoId } = req.body;
 
   try {
-    console.log(selectedTodoId);
-    console.log(targetTodoId);
-
     const updatedTodo = await todo.update(
       { title, done },
       { where: { id: id, user_id } }
     );
-    const selectedTodo = await todo.findOne({
-      where: {
-        id: selectedTodoId,
-      },
-    });
 
-    await todo.update(
-      {
-        menu_position: literal(
-          `(SELECT t.menu_position FROM todos AS t WHERE t.id = ${targetTodoId})`
-        ),
-      },
-      {
-        where: {
-          id: selectedTodoId,
-        },
-      }
-    );
-
-    await todo.update(
-      {
-        menu_position: selectedTodo.menu_position,
-      },
-      {
-        where: {
-          id: targetTodoId,
-        },
-      }
-    );
+    
     res.send(updatedTodo);
   } catch (e) {
-    console.log(e.message);
-  }
-
-  async function replaceTodo(options, user_id) {
-    const todos = await todo.findAll({
-      where: {
-        user_id,
-      },
-      order: [['menu_position', 'ASC']],
-    });
+    console.error(e.message);
   }
 });
 
